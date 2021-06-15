638d3c02b493faf0c33240c121cdaec70fe4e300
Matias Kinnunen <github@mtsknn.fi>
2021-06-15 09:08:12 +0300

    Update Episode layout

--- views/layouts/Episode.11ty.js
+++ views/layouts/Episode.11ty.js
@@ -3,14 +3,20 @@ const { html } = require('htm/preact')
 const loadComponent = require('../loadComponent')
 
 const Base = loadComponent('Base')
+const EpisodeMeta = loadComponent('EpisodeMeta')
+const MaxWidth = loadComponent('MaxWidth')
 
 module.exports = (data) => {
-  const { content, title } = data
+  const { date, description, content, recorded, title } = data
 
   return html`
     <${Base} ...${data}>
-      <h1>${title}</h1>
-      <div dangerouslySetInnerHTML=${{ __html: content }} />
+      <${MaxWidth} as="article">
+        <h1 class="font-semibold mb-3 mt-8 text-2xl">${title}</h1>
+        <${EpisodeMeta} class="mb-4" date=${date} recorded=${recorded} />
+        <p class="mb-8 text-lg">${description}</p>
+        <div dangerouslySetInnerHTML=${{ __html: content }} />
+      <//>
     <//>
   `
 }
