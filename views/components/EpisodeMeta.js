1a920f78e8fc7f9f98342e684c3fa9674461e78e
Matias Kinnunen <github@mtsknn.fi>
2021-06-15 07:03:41 +0300

    Reduce duplication

--- views/components/EpisodeMeta.js
+++ views/components/EpisodeMeta.js
@@ -2,7 +2,10 @@ const { CalendarIcon, MicrophoneIcon } = require('@heroicons/react/outline')
 const { html } = require('htm/preact')
 
 module.exports = ({ class: classes, date, recorded }) => {
-  const iconClasses = 'align-top inline h-4 w-4 mr-1 mt-1 text-gray-400'
+  const iconProps = {
+    'aria-hidden': true,
+    class: 'align-top inline h-4 w-4 mr-1 mt-1 text-gray-400',
+  }
   const format = {
     human: (date) => date.toLocaleString('fi', { dateStyle: 'short' }),
     robot: (date) => `${date.toISOString().split('T')[0]}T06:00:00+03:00`, // TODO: Daylight saving time?
@@ -12,12 +15,12 @@ module.exports = ({ class: classes, date, recorded }) => {
     <footer class="text-gray-600 ${classes}">
       <p>
         <span>
-          <${CalendarIcon} aria-hidden="true" class=${iconClasses} />
+          <${CalendarIcon} ...${iconProps} />
           <span class="sr-only">Julkaistu </span>
           <time datetime=${format.robot(date)}> ${format.human(date)} </time>
         </span>
         <span class="ml-3">
-          <${MicrophoneIcon} aria-hidden="true" class=${iconClasses} />
+          <${MicrophoneIcon} ...${iconProps} />
           <span class="sr-only"> ja äänitetty </span>
           <time datetime=${format.robot(recorded)}>
             ${format.human(recorded)}
