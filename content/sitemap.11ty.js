5062c40aded115715cbe8cf153855e61357439b4
Matias Kinnunen <github@mtsknn.fi>
2021-06-19 07:48:22 +0300

    Remove empty lines

--- content/sitemap.11ty.js
+++ content/sitemap.11ty.js
@@ -10,11 +10,9 @@ module.exports = class {
     return (
       data.collections.all
         .map((page) => page.url)
-
         // The order doesn't matter (https://stackoverflow.com/q/1274974/1079869),
         // but sorted lines look neater
         .sort()
-
         .join('\n') + '\n'
     )
   }
