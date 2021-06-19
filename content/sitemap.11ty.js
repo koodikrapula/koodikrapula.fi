b32b40e0bb8150c8191b1ed17ade167cdc4f415a
Matias Kinnunen <github@mtsknn.fi>
2021-06-19 08:04:37 +0300

    Add robots.txt, sitemap.txt and canonical tags (#34)

---
+++ content/sitemap.11ty.js
@@ -0,0 +1,19 @@
+module.exports = class {
+  data() {
+    return {
+      eleventyExcludeFromCollections: true,
+      permalink: '/sitemap.txt',
+    }
+  }
+
+  render(data) {
+    return (
+      data.collections.all
+        .map((page) => page.url)
+        // The order doesn't matter (https://stackoverflow.com/q/1274974/1079869),
+        // but sorted lines look neater
+        .sort()
+        .join('\n') + '\n'
+    )
+  }
+}
