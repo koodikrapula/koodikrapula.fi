cc189c10a68b89216edf62b0c969d5b03f99c455
Matias Kinnunen <github@mtsknn.fi>
2021-06-20 15:22:10 +0300

    Fix sitemap (#45)

--- content/episodes/episodes.11tydata.js
+++ content/episodes/episodes.11tydata.js
@@ -1,26 +1,33 @@
 const char = require('$/data/char')
 const { isProdEnv, isScheduled } = require('$/data/utils')
 
-const getAdjacentEpisode = (offset) => (data) => {
+const adjacentEpisode = (data, offset) => {
   const { episodes } = data.collections
   const { url: currentUrl } = data.page
 
   const currentIndex = episodes.findIndex(({ url }) => url === currentUrl)
   return episodes[currentIndex + offset]
 }
+const nextEpisode = (data) => adjacentEpisode(data, -1)
+const previousEpisode = (data) => adjacentEpisode(data, +1)
 
-const getPermalink = (data) =>
+const permalink = (data) =>
   isProdEnv() && isScheduled(data) ? false : `/${data.page.fileSlug}/`
 
-const getTitle = (data) =>
+function eleventyExcludeFromCollections(data) {
+  return permalink(data) === false
+}
+
+const title = (data) =>
   `${data.page.fileSlug} ${char.ndash} ${data.title}${char.nbsp}${data.emoji}`
 
 module.exports = {
   layout: 'Episode',
   eleventyComputed: {
-    nextEpisode: getAdjacentEpisode(-1),
-    permalink: getPermalink,
-    previousEpisode: getAdjacentEpisode(+1),
-    title: getTitle,
+    eleventyExcludeFromCollections,
+    nextEpisode,
+    permalink,
+    previousEpisode,
+    title,
   },
 }
