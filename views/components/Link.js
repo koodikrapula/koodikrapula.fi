6b890c40237247f41b6aa2151e8fd84aaae4d2c0
Matias Kinnunen <github@mtsknn.fi>
2021-06-20 17:43:41 +0300

    Fix style issue

--- views/components/Link.js
+++ views/components/Link.js
@@ -1,8 +1,9 @@
 const { html } = require('htm/preact')
+const { apply, tw } = require('twind')
 
 module.exports = ({ children, class: classes = '', href, ...rest }) => html`
   <a
-    class="underline text-red(hover:600 active:700) ${classes}"
+    class=${tw(classes, apply('underline text-red(hover:600 active:700)'))}
     href=${href}
     style="text-decoration-thickness: 1px"
     ...${rest}
