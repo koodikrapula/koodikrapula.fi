96c272172fbcafb6799d511e545fe725f0f29fa8
Matias Kinnunen <github@mtsknn.fi>
2021-06-20 15:18:58 +0300

    Exclude scheduled episodes from 11ty's collections

--- content/episodes/episodes.11tydata.js
+++ content/episodes/episodes.11tydata.js
@@ -14,12 +14,17 @@ const previousEpisode = (data) => adjacentEpisode(data, +1)
 const permalink = (data) =>
   isProdEnv() && isScheduled(data) ? false : `/${data.page.fileSlug}/`
 
+function eleventyExcludeFromCollections(data) {
+  return permalink(data) === false
+}
+
 const title = (data) =>
   `${data.page.fileSlug} ${char.ndash} ${data.title}${char.nbsp}${data.emoji}`
 
 module.exports = {
   layout: 'Episode',
   eleventyComputed: {
+    eleventyExcludeFromCollections,
     nextEpisode,
     permalink,
     previousEpisode,
