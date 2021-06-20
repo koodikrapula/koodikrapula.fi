1caed8d30990a9e64a78c85e9a24f39dbd80bf44
Matias Kinnunen <github@mtsknn.fi>
2021-06-20 14:54:47 +0300

    Remove `loadComponent` function

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
 require('./patchPreact')
 
 const fs = require('fs')
+const path = require('path')
 const { isValidElement } = require('preact')
 const { render } = require('preact-render-to-string')
 
@@ -58,6 +59,8 @@ module.exports = (config) => {
   // Defaults to true in Eleventy 1.0
   config.setDataDeepMerge(true)
 
+  disableViewsCache(config)
+
   return {
     dir: {
       input: 'content',
@@ -67,4 +70,35 @@ module.exports = (config) => {
       includes: '../views/layouts',
     },
   }
+}
+
+/**
+ * Disable Node.js's `require()` cache from the files in the `views` folder.
+ *
+ * Does not actually _disable_ the cache,
+ * but automatically clears the cache
+ * after any of the files are modified.
+ * So practically disables it.
+ *
+ * Needed because Node.js aggressively caches `require()`'d files.
+ * Otherwise modifying component files might not take effect.
+ * Dunno where the problem actually is: in Node.js, Browsersync or 11ty.
+ */
+function disableViewsCache(config) {
+  const viewsFolder = path.resolve('./views/')
+
+  config.on('beforeWatch', (changedFiles) => {
+    const relativePaths = changedFiles.map(path.normalize)
+
+    if (!relativePaths.find((file) => file.startsWith(`views${path.sep}`))) {
+      // No files were modified under the `views` folder
+      // -> no need to clear the cache
+      return
+    }
+
+    Object.keys(require.cache).forEach(
+      (cachePath) =>
+        cachePath.startsWith(viewsFolder) && delete require.cache[cachePath]
+    )
+  })
 }
--- views/layouts/Episode.11ty.js
+++ views/layouts/Episode.11ty.js
@@ -1,13 +1,11 @@
 const { html } = require('htm/preact')
 
 const char = require('$/data/char')
-const loadComponent = require('../loadComponent')
-
-const Base = loadComponent('Base')
-const EpisodeMeta = loadComponent('EpisodeMeta')
-const Link = loadComponent('Link')
-const Markdown = loadComponent('Markdown')
-const MaxWidth = loadComponent('MaxWidth')
+const Base = require('../components/Base')
+const EpisodeMeta = require('../components/EpisodeMeta')
+const Link = require('../components/Link')
+const Markdown = require('../components/Markdown')
+const MaxWidth = require('../components/MaxWidth')
 
 module.exports = (data) => {
   const {
