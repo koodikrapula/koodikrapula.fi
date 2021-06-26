3ddc3af6b2a8bf4a5b6ce9c3ef08a52bda030669
Matias Kinnunen <github@mtsknn.fi>
2021-06-26 16:40:36 +0300

    Install and configure ESLint

---
+++ .eslintrc.js
@@ -0,0 +1,35 @@
+/** @type {import('@types/eslint').Linter.BaseConfig} */
+module.exports = {
+  env: {
+    node: true,
+  },
+  extends: ['airbnb-base', 'prettier'],
+  ignorePatterns: ['!.eleventy.js'],
+  overrides: [
+    {
+      files: '*.11ty.js',
+      rules: {
+        'class-methods-use-this': 'off',
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 2021,
+  },
+  rules: {
+    // Allow function hoisting
+    // because sometimes it makes things clearer
+    'no-use-before-define': [
+      'error',
+      { functions: false, classes: true, variables: true },
+    ],
+
+    // Allow concatenating strings with `+`
+    // because sometimes it's clearer than template strings
+    'prefer-template': 'off',
+
+    // The following Airbnb rules need to be re-defined
+    // because the Prettier rules override them
+    quotes: ['error', 'single', { avoidEscape: true }],
+  },
+}
