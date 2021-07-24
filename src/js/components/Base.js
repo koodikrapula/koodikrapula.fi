125ab86f83045aaf5f0fe59aa46a4dfb8b32e57a
Hannes Kinnunen <20144380+Steellow@users.noreply.github.com>
2021-07-24 15:25:13 +0300

    Add source code link to footer (#74)
    * Add source code link to footer
    
    * Fix link style

--- src/js/components/Base.js
+++ src/js/components/Base.js
@@ -43,7 +43,14 @@ const Header = ({ currentUrl }) => {
 
 const Footer = () => html`
   <footer class="mt-auto py-8 text-gray-600">
-    <${MaxWidth} class="border-t-2 pt-8">${char.copy} Koodikrapula 2021<//>
+    <${MaxWidth} class="border-t-2 pt-8 flex justify-between">
+      <p>${char.copy} Koodikrapula 2021</p>
+      <p>
+        <${Link} href="https://github.com/koodikrapula/koodikrapula.fi">
+          Lähdekoodi
+        <//>
+      </p>
+    <//>
   </footer>
 `
 
