c66dedae7efd8920d2e6cf09ba4ba0432f068d60
Steellow <hankilapio@gmail.com>
2021-10-17 18:42:38 +0300

    Update link focus style

--- src/js/components/Link.js
+++ src/js/components/Link.js
@@ -3,7 +3,12 @@ import { apply, tw } from 'twind'
 
 export default ({ children, class: classes = '', href, ...rest }) => html`
   <a
-    class=${tw(classes, apply('underline text-red(hover:600 active:700)'))}
+    class=${tw(
+      classes,
+      apply(
+        'underline text-red(hover:600 active:700) focus-visible:(outline-none ring-2 ring-blue-300 border-transparent)'
+      )
+    )}
     href=${href}
     style="text-decoration-thickness: 1px"
     ...${rest}
