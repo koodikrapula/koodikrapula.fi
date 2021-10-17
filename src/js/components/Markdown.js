f564baed4b72c68a03007726ae75984ee149f266
Matias Kinnunen <github@mtsknn.fi>
2021-10-17 21:02:30 +0300

    Update todo comment

--- src/js/components/Markdown.js
+++ src/js/components/Markdown.js
@@ -1,6 +1,6 @@
 import { html } from 'htm/preact'
 
-// TODO: Rename this component e.g. to "Html"
+// TODO (#88): Rename this component e.g. to "Html"
 // because it takes HTML, not Markdown
 export default ({ content, inline = false }) => html`
   <${inline ? 'span' : 'div'} dangerouslySetInnerHTML=${{ __html: content }} />
