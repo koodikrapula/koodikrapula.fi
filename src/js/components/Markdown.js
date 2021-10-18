25c13d7ae7d529cf33aaf1441159b917fc4dec14
Steellow <hankilapio@gmail.com>
2021-10-18 15:35:03 +0300

    Try to use mila (not working)

--- src/js/components/Markdown.js
+++ src/js/components/Markdown.js
@@ -1,5 +1,7 @@
 import { html } from 'htm/preact'
 
+import md from '../utils/markdownIt'
+
 export default ({ content }) => html`
-  <div dangerouslySetInnerHTML=${{ __html: content }} />
+  <div dangerouslySetInnerHTML=${{ __html: md.render(content) }} />
 `
