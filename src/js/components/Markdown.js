083ce60f1d788ead042bd914aff8012a646b54ac
Steellow <hankilapio@gmail.com>
2021-10-25 18:58:44 +0300

    Fix md-it

--- src/js/components/Markdown.js
+++ src/js/components/Markdown.js
@@ -1,7 +1,5 @@
 import { html } from 'htm/preact'
 
-import md from '../utils/markdownIt'
-
 export default ({ content }) => html`
-  <div dangerouslySetInnerHTML=${{ __html: md.render(content) }} />
+  <div dangerouslySetInnerHTML=${{ __html: content }} />
 `
--- src/js/utils/markdownIt.js
+++ src/js/utils/markdownIt.js
@@ -1,15 +1,9 @@
 import markdownIt from 'markdown-it'
 import markdownItLinkAttributes from 'markdown-it-link-attributes'
 
-export default () => {
-  const md = markdownIt({ html: true })
-
-  md.use(markdownItLinkAttributes, {
-    attrs: {
-      class:
-        'underline text-red(hover:600 active:700) focus-visible:(outline-none ring-2 ring-blue-300 border-transparent)',
-    },
-  })
-
-  return md
-}
+export default markdownIt({ html: true }).use(markdownItLinkAttributes, {
+  attrs: {
+    class:
+      'underline text-red(hover:600 active:700) focus-visible:(outline-none ring-2 ring-blue-300 border-transparent)',
+  },
+})
